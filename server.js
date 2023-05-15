09d87ec5e30eb04d1d8bc3d052f8e0a6b9350b61
nickycraig <nickycraig@gmail.com>
2023-05-15 16:49:41 -0700

    getting started on recipe controller

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 2000;
+const recipeController = require('./controllers/recipes');
+
+const cors = require('cors');
+const morgan = require('morgan');
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Greetings earthlings');
+})
+
+app.use('', recipeController);
+
+app.get('/*', (req, res) => {
+    res.json({comment: "This is a bad URL"});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is listening to PORT ${PORT}`)
+})
